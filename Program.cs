90f4c28c71e9ad22372c99cd6bb030cf01a4379e
juldhais@gmail.com <juldhais@gmail.com>
2022-03-19 10:03:02 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Encryptor;
+
+var text = "Halo, dunia!";
+var password = "P@ssw0rd";
+
+var cipherText1 = StringEncryptor.Encrypt(text, password);
+var cipherText2 = StringEncryptor.Encrypt(text, password);
+
+var decipherText1 = StringEncryptor.Decrypt(cipherText1, password);
+var decipherText2 = StringEncryptor.Decrypt(cipherText2, password);
+
+Console.WriteLine($"Text: {text}");
+Console.WriteLine($"Password: {password}");
+Console.WriteLine($"Chipper Text #1: {cipherText1}");
+Console.WriteLine($"Chipper Text #2: {cipherText2}");
+Console.WriteLine($"Decipher Text #1: {decipherText1}");
+Console.WriteLine($"Decipher Text #2: {decipherText2}");
+
+Console.ReadKey();
---
+++ StringEncryptor.cs
@@ -0,0 +1,50 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Encryptor;
+
+public static class StringEncryptor
+{
+    public static string Encrypt(string text, string password)
+    {
+        // initialize AES and MD5
+        using var aes = Aes.Create();
+        using var md5 = MD5.Create();
+
+        // generate IV from random string
+        var rng = RandomNumberGenerator.Create();
+        var ivBytes = new byte[3];
+        rng.GetBytes(ivBytes);
+        var ivString = Convert.ToBase64String(ivBytes)[..4];
+        
+        aes.BlockSize = 128;
+        aes.Key = md5.ComputeHash(Encoding.Unicode.GetBytes(password));
+        aes.IV = md5.ComputeHash(Encoding.Unicode.GetBytes(ivString));
+
+        var bytesToEncrypt = Encoding.Unicode.GetBytes(text);
+        var encryptor = aes.CreateEncryptor();
+        var encryptedBytes = encryptor.TransformFinalBlock(bytesToEncrypt, 0, bytesToEncrypt.Length);
+        var encryptedText = Convert.ToBase64String(encryptedBytes);
+
+        return ivString + encryptedText;
+    }
+    
+    public static string Decrypt(string text, string password)
+    {
+        var iv = text[..4];
+        var encryptedText = text[4..];
+
+        using var aes = Aes.Create();
+        using var hash = MD5.Create();
+
+        aes.BlockSize = 128;
+        aes.Key = hash.ComputeHash(Encoding.Unicode.GetBytes(password));
+        aes.IV = hash.ComputeHash(Encoding.Unicode.GetBytes(iv));
+
+        var bytesToDecrypt = Convert.FromBase64String(encryptedText);
+        var decryptor = aes.CreateDecryptor();
+        var decryptedBytes = decryptor.TransformFinalBlock(bytesToDecrypt, 0, bytesToDecrypt.Length);
+
+        return Encoding.Unicode.GetString(decryptedBytes);
+    }
+}
